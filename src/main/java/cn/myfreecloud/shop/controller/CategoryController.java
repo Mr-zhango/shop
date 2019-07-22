d37660c9349fa7339adaf8834627513438e5c834
zhangyang <zhangyang@ec.e-chemchina.com>
2019-07-22 23:22:58 +0800

    解决js问题

--- src/main/java/cn/myfreecloud/shop/controller/CategoryController.java
+++ src/main/java/cn/myfreecloud/shop/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package cn.myfreecloud.shop.controller;
 
-import cn.myfreecloud.shop.basic.BaseResponse;
 import cn.myfreecloud.shop.entity.Category;
 import cn.myfreecloud.shop.service.ICategoryService;
 import com.codahale.metrics.annotation.Timed;
@@ -11,7 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -47,6 +47,16 @@ public class CategoryController {
         Optional<List<Category>> categories = Optional.of(list);
 
         modelAndView.addObject("allCategory",categories.get());
+        return modelAndView;
+    }
+
+    @Timed
+    @ApiOperation(value = "商品分类bar,查询所有的商品分类", notes = "curl -X POST \"http://127.0.0.1:8080/api/category/allCategory\" -H \"accept: application/json;charset=UTF-8\"")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/skinConfig", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ModelAndView skinConfig() {
+        //视图和模型对象,初始化的时候就设置页面的地址
+        ModelAndView modelAndView = new ModelAndView("skin-config");
         return modelAndView;
     }
 }
